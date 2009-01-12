b392c5ba533fe1f9fb12ef080c1ea7022bbb77e9
Fabian Wuertz <xadras@users.berlios.de>
2009-01-12 08:50:32 +0000

    * installer: de translation updated * debian: wget dependency added

--- eduversum-installer/eduversum-installer_de.ts
+++ eduversum-installer/eduversum-installer_de.ts
@@ -1,104 +1,106 @@
-<!DOCTYPE TS><TS>
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1" language="de">
+<defaultcodec></defaultcodec>
 <context>
     <name>MainWindow</name>
     <message>
         <source>Welcome</source>
-        <translation type="unfinished"></translation>
+        <translation>Willkommen</translation>
     </message>
     <message>
         <source>Resynchronize the package index files</source>
-        <translation type="unfinished"></translation>
+        <translation>Paketindex neu einlesen </translation>
     </message>
     <message>
         <source>Remove packages</source>
-        <translation type="unfinished"></translation>
+        <translation>Entferne Pakete</translation>
     </message>
     <message>
         <source>Please wait...</source>
-        <translation type="unfinished"></translation>
+        <translation>Bitte warten...</translation>
     </message>
     <message>
         <source>Install packages</source>
-        <translation type="unfinished"></translation>
+        <translation>Installiere Pakete</translation>
     </message>
     <message>
         <source>Download packages</source>
-        <translation type="unfinished"></translation>
+        <translation>Lade Pakete herunter</translation>
     </message>
     <message>
         <source>Process finished</source>
-        <translation type="unfinished"></translation>
+        <translation>Aktion beendet</translation>
     </message>
     <message>
         <source>close</source>
-        <translation type="unfinished"></translation>
+        <translation>schliessen</translation>
     </message>
     <message>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Fehler</translation>
     </message>
     <message>
         <source>It is not allowed to change xserver packages during the xserver is running!</source>
-        <translation type="unfinished"></translation>
+        <translation>Es ist nicht erlaubt Pakete des X-Server zu ändern während dieser läuft</translation>
     </message>
     <message>
         <source>Packages to install</source>
-        <translation type="unfinished"></translation>
+        <translation>zu installierende Pakete</translation>
     </message>
     <message>
         <source>Packages to update</source>
-        <translation type="unfinished"></translation>
+        <translation>zu aktualisierende Pakete</translation>
     </message>
     <message>
         <source>Packages to remove</source>
-        <translation type="unfinished"></translation>
+        <translation>zu entfernende Pakete</translation>
     </message>
     <message>
         <source>Downloading</source>
-        <translation type="unfinished"></translation>
+        <translation>herunterladend...</translation>
     </message>
     <message>
         <source>show details</source>
-        <translation type="unfinished"></translation>
+        <translation>Details einblenden</translation>
     </message>
     <message>
         <source>hide details</source>
-        <translation type="unfinished"></translation>
+        <translation>Details ausblenden</translation>
     </message>
 </context>
 <context>
     <name>eduversumGui</name>
     <message>
         <source>Eduversum Installer</source>
-        <translation type="unfinished"></translation>
+        <translation>Eduversum Installer</translation>
     </message>
     <message>
         <source>about</source>
-        <translation type="unfinished"></translation>
+        <translation>über</translation>
     </message>
     <message>
         <source>next </source>
-        <translation type="unfinished"></translation>
+        <translation>weiter</translation>
     </message>
     <message>
         <source>Welcome</source>
-        <translation type="unfinished"></translation>
+        <translation>Willkommen</translation>
     </message>
     <message>
         <source>Res&amp;ynchronize the package index files (apt-get update)</source>
-        <translation type="unfinished"></translation>
+        <translation>Paketindex neu einlesen (apt-get update)</translation>
     </message>
     <message>
         <source>Skip Resynchroni&amp;zation</source>
-        <translation type="unfinished"></translation>
+        <translation>Neueinlesen überspringen</translation>
     </message>
     <message>
         <source>show details</source>
-        <translation type="unfinished"></translation>
+        <translation>zeige Details</translation>
     </message>
     <message>
         <source>1</source>
-        <translation type="unfinished"></translation>
+        <translation>1</translation>
     </message>
     <message>
         <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
@@ -107,7 +109,12 @@ p, li { white-space: pre-wrap; }
 &lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Arial&apos;; font-size:11pt; font-weight:400; font-style:normal;&quot;&gt;
 &lt;table&gt;&lt;tr&gt;&lt;td&gt;&lt;img src=&quot;/usr/share/eduversum-installer/icons/warning.png&quot;&gt;&lt;/td&gt;&lt;td&gt;
 &lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;Warning: Because of dependencies there will be more packages (red marked) removed, then you have chosen.&lt;/p&gt;&lt;/td&gt;&lt;/tr&gt;&lt;/table&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;(new line)
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;(new line)
+p, li { white-space: pre-wrap; }(new line)
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Arial&apos;; font-size:11pt; font-weight:400; font-style:normal;&quot;&gt;(new line)
+&lt;table&gt;&lt;tr&gt;&lt;td&gt;&lt;img src=&quot;/usr/share/eduversum-installer/icons/warning.png&quot;&gt;&lt;/td&gt;&lt;td&gt;(new line)
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;Warnung: Wegen Abhängigkeiten werden mehr Pakete entfernt (rot markiert) als Sie ausgewählt haben.&lt;/p&gt;&lt;/td&gt;&lt;/tr&gt;&lt;/table&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
@@ -120,27 +127,36 @@ p, li { white-space: pre-wrap; }
 &lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;img src=&quot;/usr/share/eduversum-installer/icons/hint.png&quot; /&gt;&lt;/p&gt;&lt;/td&gt;
 &lt;td&gt;
 &lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;Note: Because of dependencies there will be more packages (orange marked) installed/updated, then you have chosen.&lt;/p&gt;&lt;/td&gt;&lt;/tr&gt;&lt;/table&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;(new line)
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;(new line)
+p, li { white-space: pre-wrap; }(new line)
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Arial&apos;; font-size:11pt; font-weight:400; font-style:normal;&quot;&gt;(new line)
+&lt;table border=&quot;0&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px;&quot; cellspacing=&quot;2&quot; cellpadding=&quot;0&quot;&gt;(new line)
+&lt;tr&gt;(new line)
+&lt;td&gt;(new line)
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;img src=&quot;/usr/share/eduversum-installer/icons/hint.png&quot; /&gt;&lt;/p&gt;&lt;/td&gt;(new line)
+&lt;td&gt;(new line)
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;Achtung: Wegen Abhängigkeiten werden mehr Pakete installiert (orange markiert) als Sie gewählt haben.&lt;/p&gt;&lt;/td&gt;&lt;/tr&gt;&lt;/table&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <source>Download</source>
-        <translation type="unfinished"></translation>
+        <translation>Download</translation>
     </message>
     <message>
         <source>*</source>
-        <translation type="unfinished"></translation>
+        <translation>*</translation>
     </message>
     <message>
         <source>Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Name</translation>
     </message>
     <message>
         <source>Version</source>
-        <translation type="unfinished"></translation>
+        <translation>Version</translation>
     </message>
     <message>
         <source>Source</source>
-        <translation type="unfinished"></translation>
+        <translation>Quelle</translation>
     </message>
 </context>
 </TS>
